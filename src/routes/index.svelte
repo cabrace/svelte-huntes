01bc6a923e72d9fb96f8da882fd0cd3d437fcc75
cb <christopher.a.brace@gmail.com>
2022-07-02 23:32:45 -0400

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+<html class="-red">
+
+
+		<script async defer src="https://www.jscache.com/wejs?wtype=cdsscrollingravenarrow&amp;uniq=267&amp;locationId=1868421&amp;lang=en_US&amp;border=true&amp;shadow=true&amp;display_version=2" data-turbolinks-track="reload"></script>
+		<meta name="turbolinks-cache-control" content="no-cache">
+	<link rel="icon" type="image/x-icon" href="/assets/orchid-e0b2fbfde4a96726fc3c058e05dcf067e189d2ec9432ebd33fb5716db5f0ea3a.ico" />
+
+	<!--Conditional head loading-->
+			<head>
+	<title>Huntes Gardens Barbados| The Most Enchanting Place On Earth. </title>
+	<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="i+Nf7M3MCegsK1/G9cVN4CHJPh+kbJhDrwfohFeQj5+4aFUBs2jWrLaNrPNpZydHB0nI77P/qO8IMoWGa6K0iQ==" />
+	<link rel="stylesheet" media="all" href="/assets/application-3d8e6a2c30b08034439d75a9776b676df9dd489595466a8b2048bad0e73ccec6.css" data-turbolinks-track="reload" />
+	<script src="/assets/application-cf4a95db4977929a7ec92523d4185c131dbe6591956f6723ed7934514771a41f.js" data-turbolinks-track="reload"></script>
+	<meta name="keywords" content="Hunte's Gardens, Barbados, Caribbean, vacation, attractions, nature, eco, exotic plants, nursery">
+	<meta name="description" content="Hunte's Gardens and Nursery. One of Barbados' must-see attractions - lush and unusual garden, located between Welchman Hall Gully and Flower Forest.">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="turbolinks-visit-control" content="reload">
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Serif" rel="stylesheet">
+	<!--Trip Advisor-->
+	<script async src="https://www.jscache.com/wejs?wtype=cdsscrollingravenarrow&amp;uniq=267&amp;locationId=1868421&amp;lang=en_US&amp;border=true&amp;shadow=true&amp;display_version=2" data-turbolinks-track="reload" ></script>
+
+</head>
+
+	<body class="site flexbox column between -blue">
+		
+<div class="banner__home">
+	<header>                   	
+	<div class="nav navigation">
+
+		<div class="menu">
+			<div class="header__nav-link"><a href="/">Home</a></div>
+			<!--<div class="header__nav-link"><a href="/">About</a></div>-->
+			<div class="header__nav-link"><a href="/gallery">Gallery</a></div>
+			<div class="header__nav-link"><a href="/directions">Directions</a></div>
+		</div>
+
+		<!--Mobile Menu-->
+		<div id="menuToggle">
+			<label for="toggle">
+				<input id="toggle" type="checkbox" /> 
+				<div class="burger">
+					<span class="burger__text">Menu</span>
+					<div class="burger-icon">			
+						<span class="stripe"></span>
+						<span class="stripe"></span>
+						<span class="stripe"></span>
+					</div>
+				</div>
+				<div class="menu">
+					<div class="header__nav-link"><a href="/">Home</a></div>
+					<div class="header__nav-link"><a href="/gallery">Gallery</a></div>
+					<div class="header__nav-link"><a href="/directions">Directions</a></div>
+				</div>
+			</label>
+
+		</div>
+	</div>
+
+</header>  
+
+	<div class="banner__home">
+		<h1 class="h1 logo">Hunte's Gardens</h1> 
+		<div class="slogan"><q>The most enchanting place on Earth.</q></div>
+	</div>
+</div>
+
+<!--==============================content================================-->
+
+<div class="content">
+
+  <!--NOTICE -->
+	<!--<section class="below-banner">--> 
+		<!--<div class="below-banner__text__intro">-->
+
+			<!--<div class="below-banner__text__intro__inner", style="max-width:650px; display:inline-block; text-align:center;">-->
+
+			<!--<h2 class="section__heading", style="color:#f7c825; text-align:left; margin-left: -0.5%;">Important Notice: </h2>-->
+				<!--<span style="text-align:left;">-->
+					<!--<p>Temporary Closure of Hunte’s Gardens, Barbados</p><br/>-->
+
+          <!--<p>Dear friends,</p><br/>-->
+
+					<!--<p>-->
+					<!--We want to do our bit to protect the community from Coronavirus. So, we have decided to-->
+					<!--close the gardens temporarily from Sunday 22 nd March. We will reopen as soon as it is safe-->
+					<!--for our visitors.-->
+					<!--</p><br/>-->
+
+					<!--<p>-->
+					<!--In the meantime, we will be working behind closed doors. We promise to surprise and-->
+					<!--delight you on your next visit!-->
+					<!--</p><br/>-->
+
+					<!--<p>-->
+					<!--We would also like to send a very special message of thanks and appreciation to all of those-->
+					<!--who have supported the gardens since we opened in 2007.-->
+					<!--</p><br/>-->
+
+					<!--<p>-->
+					<!--We hope that you will recall time spent in the gardens to calm the soul during these difficult-->
+					<!--times. If you would like to contribute, post your pictures of the gardens on our Facebook-->
+					<!--page or on Twitter. Start a conversation with other garden lovers and share your-->
+					<!--experiences of the most enchanting place on earth!-->
+					<!--</p><br/>-->
+
+					<!--<p>-->
+					<!--Thank you again, stay safe and we look forward to seeing you all back in beautiful Barbados-->
+					<!--and at Hunte’s gardens very soon!-->
+					<!--</p><br/>-->
+
+					<!--<p>Anthony Hunte.</p>-->
+
+				<!--</span>-->
+			<!--</div>-->
+		<!--</div>-->
+	<!--</section>-->
+	<!-- Temopraarily removed below -->
+
+  <section class="below-banner">
+    <div class="below-banner__text__intro">
+
+      <div class="below-banner__text__intro__inner">
+			Hunte's Gardens in Barbados is A place of serenity where you can feel <span class="accent--purple">peace of mind</span> and the <span class="accent--tranquil">tranquility</span> of your soul whilst being surrounded by <span class="accent--green">nature</span> in some of the wonderous varieties it has to offer. Take a leisurely stroll through the tall palm trees of what once was a regular gully; which has now become a majestic transformation by Anthony Hunte into an ornately designed garden located in Barbados, known by many around the world, simply as: <span class="accent--yellow">Hunte's Gardens</span>.
+      </div>
+    </div>
+  </section>
+
+	<section class="layer--light highlights">
+		<!--<div class="highlights__heading">What's there to do<span class="hux">?</span></div>-->
+		<h2 class="section__heading">Things to do</h2>
+
+		<!-- NOTE : Re-write this section to accomodate css-grid fallbacks. structure it that way -->
+		<div class="highlights__grid">
+
+			<div class="highlights__grid__image-1 "><img src="/assets/highlight-1-2235e7085362a1d0c14b32cab53c050b8e665f59cc9007a6a2331b18ff484866.jpg" alt="Highlight 1" /> </div>
+			<div class="highlights__grid__info-1">
+				<h3 class="highlights__grid__title">Explore</h3>
+				<div class="highlights__grid__text">
+					<!--<h6>-B-1-</h6>-->
+					A picturesque and easy-to-walk path offers many surprises, thanks to the superbly created series of mini-gardens embedded within the main large unit, combining vibrant colours and textures of rare exotic plants to offer the visitor a most uplifting and fulfilling experience. </div>
+			</div>
+
+
+			<div class=" highlights__grid__image-2 "><img src="/assets/highlight-2-6e045d5ab3a6a4a2a9c8b525a03d080a3722de65d056e162ec1b590b81277e80.jpg" alt="Highlight 2" /> </div>
+			<div class="highlights__grid__info-2">
+				<h3 class="highlights__grid__title">Discover</h3>
+				<div class="highlights__grid__text"> 
+					<!--<h6>-B-2-</h6>-->
+					Challenge yourself to spot and identify up to 84 of the listed items as found on the large board - located at the bottom of the garden paths and many of the more unlisted ones as you will! A printed handout copy is also available for reference as you walk. </div>
+			</div>
+
+			<div class=" highlights__grid__image-3 "><img src="/assets/highlights-relax-3-a3709ea4ddec65206319b0cd959cab07d58ab801373f6320cee810d155c142e8.jpg" alt="Highlights relax 3" /> </div>
+			<div class="highlights__grid__info-3">
+				<h3 class="highlights__grid__title">Relax</h3>
+				<div class="highlights__grid__text">
+					<!--<h6>-B-3-</h6>-->
+					You can find your privacy and relax on one of the garden benches cosily hidden among the greenery as you go at your own pace in this self-guided tour of enchantment with botany and nature in this relatively space. Also meet and converse with others at the top house.</div>
+			</div>
+
+			<div class=" highlights__grid__image-4 "><img src="/assets/highlight-4-eed87bccb407e5e26ea463297acfc417f595ca385bccb68864b0c77787620a49.jpg" alt="Highlight 4" /> </div>
+			<div class="highlights__grid__info-4">
+				<h3 class="highlights__grid__title">Refreshments</h3>
+				<div class="highlights__grid__text">
+					<!--<h6>-B-4-</h6>-->
+					Meet the creator of this gem of a place: Anthony Hunte as you can be sure to be welcomed with fascinating stories, whilst enjoying refreshments such as: a glass of rum punch, selection of teas, coffees and cakes which can additionally be purchased.</div>
+			</div>
+
+		</div>
+	</section>
+
+
+	<section style="margin:70px 0px;">
+		<div style="display:flex;"><a class="gallery-link__button" href="/gallery">View our photo gallery</a></div>
+	</section>
+
+	<section class="occasions"> 
+
+		<div class="occasions__block">
+			<!--<div class="occasions__block__heading">Have a special occasion<span class="hux">?</span></div>-->
+			<h2 class="section__heading">Special occasions</h2>
+			<div class="occasions__block__tex">
+				<div class="occasions__block__text__inner">
+
+					<!--<h6>-C-</h6>-->
+					<!--<p style='text-align:center;'>Call to discuss a special event as we do host: </p>-->
+
+					<ul class="occasions">
+						<li>
+							<div class="gallery-link__leaf">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="69.9 18.4 191 191" role="img" preserveAspectRatio="xMidYMid meet">
+									<g>
+									<path class="fill" d="M259.3 85.5s-40.5-18.9-75-4.6c-48.5 20.1-35.2 78.3-22.8 51.6 12.4-26.8 40.5-43.1 60.7-43.1 0 0-43.1 17-54.8 44.4-11.7 27.4 11.7 32.6 22.2 32.6 10.4 0 28.1-3.9 41.8-40.5 13.7-36.5 13.7-38.5 28.1-40.5z"/>
+									<path class="fill" d="M97.5 133.2s37.2 2 45.7 8.5 2 17.6-5.2 22.2c-7.2 4.6-23.5 4.6-32-1.3s-22.8-14.4-34.6-12.4c0 0 8.5-17 23.5-25.4 15-8.5 43.1-3.9 47 1.3 5 6.7 2 8.5-9.1 6.5-11.1-2-32-2-35.2z"/>
+									<path class="fill" d="M114.5 60.8s18.3 3.3 26.8 11.7c8.5 8.5 10.4 13.1 10.4 21.5s-7.2 22.8-9.8 6.5c-2.6-16.3-1.3-13.7-5.2-18.9 0 0 9.4 34.6-5.9 32-18.9-3.3-14.4-18.9-14.4-30s2.6-17.6-2-22.8z"/>
+									</g>
+								</svg>
+							</div>
+						</li>
+						<li class="occasion" >Weddings</li>
+						<li class="occasion" >Special celebrations</li>
+						<li class="occasion" >Anniversary parties</li>
+						<li class="occasion" >Artistic Photo-shooting sessions</li>
+						<li class="occasion" >Special event of your choice...</li>
+					</ul>
+
+					<p class="occasions__info"> &nbsp;
+					<span style="color:#f7c825;">Call to discuss any special event, </span> <br/>as we can accommodate small to medium-sized parties in our unusual, bohemian dining area located directly in the Garden.
+					</p>
+
+				</div> 
+			</div>
+		</div>
+
+		<!--Experimental setup-->
+		<!--<div class="occasions__block">-->
+		<!--<div class="location__block__heading">Centrally Located</div>-->
+		<!--<div class="location__block__text">-->
+		<!--<div class="location__block__text__inner">-->
+		<!--<h6>-D-</h6>-->
+		<!--Hunte's Gardens, centrally located in lush hills of St. Joseph's, are conveniently accessible from all around Barbados. Just 20 to 30 minutes' drive from Holetown, or St. Lawrence Gap, not far from Flower Forest and Orchid World, they offer to garden and nature lovers one of the finest experiences one can find on East Caribbean Islands.-->
+		<!--</div>-->
+		<!--</div>-->
+		<!--</div>-->
+	</section>
+
+	<!--<section style="display:flex; flex-direction:row;">	-->
+		<!--<video id="vjs-player" controls="controls" class="video-js" style="margin:0 auto;" width="500" height="300" src="/assets/huntes_gardens-fd3535eba1dc0ffe1cd3e8ed5f7eae7bd7ba5f0362ac6e98498ac8cd31debfc6.mov"></video>-->
+	<!--</section>	-->
+	
+	<!--<video poster="/path/to/poster.jpg" id="player" playsinline controls>-->
+    <!--<source src="/videos/huntes_gardens.mov" type="video/mp4" />-->
+    <!--<source src="/path/to/video.webm" type="video/webm" />-->
+    <!--[> Captions are optional <]-->
+    <!--[><track kind="captions" label="English captions" src="/path/to/captions.vtt" srclang="en" default /><]-->
+<!--</video>-->
+
+	<section class="location">
+		<div class="location__block">
+			<!--<div class="location__block__heading">How to get there<span class="hux">?</span></div>-->
+			<h2 class="section__heading">Centrally Located</h2>
+			<div class="location__block__text">
+				<div class="location__block__text__inner">
+					<!--<h6>-D-</h6>-->
+					Hunte's Gardens, centrally located in lush hills of St. Joseph's, are conveniently accessible from all around Barbados. Just 20 to 30 minutes' drive from Holetown, or St. Lawrence Gap, not far from Flower Forest and Orchid World, they offer to garden and nature lovers one of the finest experiences one can find on East Caribbean Islands.
+				</div>
+			</div>
+
+		<div style="display:flex;"><a class="location__block__button" href="/directions">Get directions</a></div>
+						<!--<form class="button_to" method="get" action="/directions"><input class="location__block__button" type="submit" value="Get directions"></form>-->
+		</div>
+	</section>
+
+	<section>
+
+	</section>
+	<div class="flexbox row">
+
+		<!--<section class=" full-width flexbox column between info map">-->
+
+		<!--<div id="map"></div>-->
+
+		<!--<script>-->
+			<!--var map, map2;-->
+
+	<!--function initMap() {-->
+	<!--var huntes = {lat: 13.193377, lng: -59.551091};-->
+
+	<!--var options = {-->
+	<!--zoom: 15,-->
+	<!--center: huntes-->
+	<!--};-->
+
+	<!--var map = new google.maps.Map(document.getElementById('map'), options);-->
+
+	<!--var marker = new google.maps.Marker({-->
+	<!--position: huntes,-->
+	<!--map: map-->
+	<!--});-->
+	<!--}-->
+	<!--</script>-->
+
+		<!--</section>-->
+
+	</div>
+</div>
+
+
+
+		<!--This section we need organized columns -->
+
+			<footer>
+
+					<div style="width: 90%; margin: 0 auto;" id="TA_cdsscrollingravenarrow267" class="TA_cdsscrollingravenarrow index"><ul id="IoLXHxCS1mVO" class="TA_links PO0QfI"><li id="YUCvHgTah" class="v6dM7zfGAVao"><a target="_blank" href="https://www.tripadvisor.com/"><img src="https://static.tacdn.com/img2/t4b/Stacked_TA_logo.png" alt="TripAdvisor" class="widEXCIMG" id="CDSWIDEXCLOGO"/></a></li></ul></div>
+
+
+	<div class="location__social">
+
+		<div class="location__block__heading">Visit and experience for yourself!</div>
+		<div class="location__info">
+			
+			<div>
+				<div class="location__info__admission">
+					<!--<h6>-D-1</h6>-->
+					<div class="location__info__admission--pricing">Admission: $20 USD / $40 BBD Per Person.</div>
+					<div><p>Children under 12 half price </p></div>
+					<div><p>( Cash only )</p></div>
+					<div class="location__info__admission--pricing">Opening Hours: 10 a.m. - 4 p.m.</div>
+					<!-- <div><p style="">From 1st Nov 2022 (9 a.m - 5 p.m)</p></div></br> -->
+          <div><p style="color:#f7c825">EVERY DAY</p></div><!--</br>-->
+          <div><p> ( including SUNDAYS and BANK HOLIDAYS ) </p></div>
+					<!--<div class="location__info__admission--pricing"><span style="color:red;"> closing from this sunday(22/03/2020) until further notice.</span> </div>-->
+					<!--<div><p>(Including holidays)</p></div>-->
+					<!--<div class="location__info__admission--pricing"><p style="font-size:large;"> &gt;&gt; closed every Sunday until further notice &lt;&lt;</p></div>-->
+				</div>
+
+				<section>
+					<div>
+						<p>Follow us on</p>
+					</div>
+				<div style="border:0px dashed white; border-radius:8px; padding:18px 0px; display:flex; flex-direction:row; justify-content:space-around;">
+					<div><a href="https://www.facebook.com/huntesgarden"><span><i class="icon-facebook2" style="font-size:35px;"> </i></span></a></div>	
+					<div><a href="https://www.instagram.com/huntesgardens/"><span><i class="icon-instagram" style="font-size:35px;"> </i></span></a></div>
+				<div><a href="https://www.tripadvisor.com/Attraction_Review-g1183257-d1868421-Reviews-Hunte_s_Gardens-Saint_Joseph_Parish_Barbados.html"><span><i class="icon-tripadvisor" style="font-size:35px;"> </i></span> </a></div>
+				</div>
+				<!--<h4 style="text-align:center;">Follow us</h4>-->
+						<!--<form class="button_to" method="get" action="/directions"><input class="footer__info__button" type="submit" value="Get directions"></form>-->
+				</section>
+				<!--<form class="button_to" method="get" action="/directions"><input class="location__info__button" type="submit" value="get directions here" /></form>-->
+							</div>					
+
+			<div class="location__info__image">
+				<!--<svg width="100" height="100" version="1.1" viewBox="0 0 102.83552 102.83551" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:osb="http://www.openswatchbook.org/uri/2009/osb"><g transform="translate(2.39 -409.55)" fill="#0083d7" fill-opacity=".2449" stroke="#000" stroke-width="2"><g transform="matrix(.19533 0 0 .19533 -.78236 411.16)" fill="#ff794b" fill-opacity="1" stroke="#000" stroke-width="3.0461"><path d="m502.35 237.15-229.5-229.5c-10.199-10.2-25.5-10.2-35.7 0l-229.5 229.5c-10.2 10.2-10.2 25.501 0 35.7l229.5 229.5c10.2 10.2 25.501 10.2 35.7 0l229.5-229.5c10.2-10.199 10.2-25.499 0-35.7zm-196.35 81.601v-63.75h-102v76.5h-51v-102c0-15.3 10.2-25.5 25.5-25.5h127.5v-63.75l89.25 89.25-89.25 89.25z" fill="#c8db1c" fill-opacity="1" stroke="#000" stroke-width="6.3993"/></g></g></svg>-->
+				<img width="180px" class="info__directions__image" src="/assets/signpost-red-white-7486d244da41a06ec24fa2f9143ea5bfd910db69412e284dbaca02e5dc6cbcb9.svg" alt="Signpost red white" />
+			</div>
+
+			<div class="footer__column" >
+				<div>
+					<h4>Contact</h4>
+					<div class="flex-row" >
+						<p><span><i class="icon-home"> Address </i></span>Hunte's Gardens
+						Castle Grant, St. Joseph, Barbados, W.I.</p>
+						<p><span><i class="icon-email"> Email </i></span> huntesgardens@gmail.com</p>
+						<p><span><i class="icon-phone"> Phone</i></span> <a href="tel:+1-246-433-3333">(246) 433-3333</a> </p>
+						<p><span><i class="icon-location2"> Geolocation</i></span> <br/> 
+						13°11'35.7"N 
+						59°33'04.6"W
+						</p>
+
+					</div>
+				</div>
+
+			</div>
+			<!--<div class="location__info__button">Click here for directions</div>-->
+			<!--<form class="button_to" method="get" action="/directions"><input class="location__info__button" type="submit" value="get directions here" /></form>-->
+
+		</div>
+
+	</div>
+
+	<div class="footer__copyright" style="text-align:left;" >
+
+		<div class="footer__copyright__text ">
+			<p>
+		&copy; Huntes Gardens 2019
+			</p>
+		</div>
+	</div>
+
+</footer>
+
+</body>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136740387-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-136740387-1');
+</script>
+</html>
+
